101be3e99dab34fefa0db6ccead31f162ae20655
Souman Paul <souman1729@gmail.com>
2019-11-20 17:07:48 +0530

    Components Part 1

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Main />
+      // <Text>Hi its my mobile app</Text>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
