d9744cb4e3228ccf84a573d9201a0d693335a5de
soumanpaul <souman1729@gmail.com>
2022-01-20 23:51:56 +0530

    updated redux store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,37 +1,34 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { dishes } from './dishes';
-import { comments } from './comments';
-import { promotions } from './promotions';
-import { leaders } from './leaders';
-import { favorites } from './favorites';
-import { persistStore, persistCombineReducers } from 'redux-persist';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { dishes } from "./dishes";
+import { comments } from "./comments";
+import { promotions } from "./promotions";
+import { leaders } from "./leaders";
+import { favorites } from "./favorites";
+import { persistStore, persistCombineReducers } from "redux-persist";
 // import storage from 'redux-persist/lib/storage';
 // import AsyncStorage from '@react-native-community/async-storage';
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const config = {
-    key: 'root',
-    storage: AsyncStorage,
-    debug: true
-  }
+  key: "root",
+  storage: AsyncStorage,
+  debug: true,
+};
 
 export const ConfigureStore = () => {
-    
- 
+  const store = createStore(
+    persistCombineReducers(config, {
+      dishes,
+      comments,
+      promotions,
+      leaders,
+      favorites,
+    }),
+    applyMiddleware(thunk, logger)
+  );
+  const persistor = persistStore(store);
 
-    const store = createStore(
-        persistCombineReducers(config, {
-            dishes,
-            comments,
-            promotions,
-            leaders,
-            favorites
-        }),
-        applyMiddleware(thunk, logger)
-    );
-    const persistor = persistStore(store)
-    
-    return { persistor,  store};
-}
+  return { persistor, store };
+};
